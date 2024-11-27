cd9a2bf03a0aa7c4ec88d792f206ed3fdddbe8a0
Raphael Matias <phael110199@gmail.com>
2024-11-27 19:43:00 -0300

    Add slug, updated_at and is_available field to Product model

--- products/models.py
+++ products/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='products', null=True, blank=True)
-    created_at =models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    @property
+    def is_available(self):
+        return self.stock > 0
