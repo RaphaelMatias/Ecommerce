3189bfd9af2e3e42a5142fb15534d090425dd64f
Raphael Matias <phael110199@gmail.com>
2024-11-27 20:01:20 -0300

    Create Category model with fields for name and description and category FK to Products

--- products/models.py
+++ products/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.utils.text import slugify
 from django.core.validators import MinValueValidator
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.SET_NULL, null=True, blank=True)
     slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
