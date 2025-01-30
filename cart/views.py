fdc2890bc61e9dfc691ea5a42c725aa5e8769c22
Raphael Matias <phael110199@gmail.com>
2025-01-30 15:08:38 -0300

    feat(cart): add serializers and views for cart and cart items

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Cart, CartItem
+from .serializers import CartSerializer
 
-# Create your views here.
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        serializer = self.get_serializer(cart)
+        return Response(serializer.data, status=status.HTTP_200_OK if not created else status.HTTP_201_CREATED)
