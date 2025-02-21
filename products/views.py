9cf01103ff306e990e57101dd7ebe374c511cc79
Raphael Matias <phael110199@gmail.com>
2025-02-21 17:28:45 -0300

    Refactor(product): improve related products logic with hierarchical prioritization

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
-from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import Q
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.db.models import Q, Case, When, IntegerField, Subquery, OuterRef
+from django.db.models.functions import Coalesce
 from .models import Product, Category
 from .serializers import ProductSerializer
 
@@ -18,35 +19,50 @@ class ProductViewSet(viewsets.ModelViewSet):
     permission_classes = [IsStaffOrReadOnly]
 
     def get_queryset(self):
-        if self.request.user.is_authenticated and self.request.user.is_staff:
-            return Product.objects.all()
-        return Product.objects.filter(is_active=True, stock__gt=0)
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(is_active=True, stock__gt=0)
+        return queryset.select_related('category', 'brand')
 
-    def get_related_categories(self, category):
-        if not category:
-            return []
-
-        return Category.objects.filter(
-            Q(tree_id=category.tree_id) &
-            Q(lft__range=(category.lft, category.rght))
-        )
-
-    @action(detail=True, methods=['get'])
-    def related(self, request, pk=None):
+    @action(detail=True, methods=['get'], url_path='related')
+    def related_products(self, request, pk=None):
         product = self.get_object()
-        main_category = product.category
-
-        related_categories = self.get_related_categories(main_category)
-        related_products = Product.objects.filter(
-            Q(category__in=related_categories) | 
-            Q(related_products=product)
-        ).exclude(id=product.id).distinct().order_by('?')
-
-        same_category = related_products.filter(category=main_category)[:4]
-        other_related = related_products.exclude(category=main_category)[:4]
-
-        final_products = list(same_category) + list(other_related)
-        final_products = final_products[:8]
-
-        serializer = self.get_serializer(final_products, many=True)
+        related_products = self._get_related_products(product)
+        serializer = self.get_serializer(related_products, many=True)
         return Response(serializer.data)
+
+    def _get_related_products(self, product):
+        base_qs = Product.objects.filter(is_active=True).exclude(id=product.id)
+        parent_category = product.category.parent if product.category else None
+
+        return base_qs.annotate(
+            manual_relation=Case(
+                When(related_products=product, then=0),
+                default=1,
+                output_field=IntegerField()
+            ),
+            hierarchy_group=Case(
+                When(category=product.category, then=0),
+                When(
+                    Q(category=parent_category) | 
+                    Q(category__parent=parent_category) if parent_category 
+                    else Q(pk__isnull=False),
+                    then=1
+                ),
+                default=2,
+                output_field=IntegerField()
+            ),
+            category_depth=Coalesce(
+                Subquery(
+                    Category.objects.filter(pk=OuterRef('category'))
+                    .values('level')[:1]
+                ), 
+                0
+            )
+        ).filter(
+            hierarchy_group__in=[0, 1]  # Remove totalmente o fallback
+        ).order_by(
+            'manual_relation',
+            'hierarchy_group',
+            '-category_depth'
+        )[:8]  # Pode retornar menos de 8 se não houver produtos suficientes
